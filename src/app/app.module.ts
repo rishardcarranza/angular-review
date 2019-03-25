07cdafc8921900b2c06cbca2f8b9c1a756ad99c1
Richard Carranza <rishardcarranza@gmail.com>
2019-03-24 21:30:17 -0600

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './components/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // AboutComponent,
+    // ContactComponent,
+    // HomeComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/posts/posts.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+
+    this.dataService.getPosts()
+      .subscribe(posts => {
+        console.log(posts);
+      });
+
+  }
+
+}
